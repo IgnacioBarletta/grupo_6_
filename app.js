02f4443ba2b8f3fe2edb8a5bb31a5bea88d44254
Ignacio Barletta Trípodi <IgnacioBarletta@users.noreply.github.com>
2021-05-09 21:40:58 -0300

    Subo login.html, register.html y sus CSS

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-
 const publicpath = path.resolve(__dirname, './public');
+
 app.use(express.static(publicpath));
 
 app.listen(3000, () => {
@@ -12,4 +11,16 @@ app.listen(3000, () => {
 
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
+});
+
+app.get('/productDetail', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+});
+
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
 });
