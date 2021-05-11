9c21af76c02063f1f5a7c4d3319dddefe1057d09
lorenavluna <lorenavluna@gmail.com>
2021-05-11 20:22:34 -0300

    subo el index y el css

--- app.js
+++ app.js
@@ -13,3 +13,16 @@ app.listen(3000, () => {
 app.get('/', (req,res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
+
+app.get('/productDetail', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+});
+app.get('/login', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+app.get('/register', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+app.get('/index-copy', (req,res) => {
+    res.sendFile(path.resolve(__dirname, './views/index-copy.html'));
+});
